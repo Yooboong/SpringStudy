f89baeb401e5129c426846d0fd49ce30529724bc
Yooboong <yooboong22@gmail.com>
2024-05-24 15:36:06 +0900

    조회(Read) 기능 구현

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j //로깅 기능을 위한 어노테이션 추가(lombok 사용시) 로그를 찍을때는 log.info()문 사용
 @Controller
@@ -34,6 +38,34 @@ public class ArticleController {
         log.info(saved.toString()); //로깅 코드 추가
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}") //중괄호{} 안의 값은 변수로 사용한다는 뜻
+    public String show(@PathVariable Long id, Model model) { //@PathVariable은 url 요청으로 들어온 전달 값을 컨트롤러의 매개변수로 가져오는 어노테이션
+        //매개변수로 id 받아오기
+
+        log.info("id = " + id); //id를 잘 받았는지 확인하는 로그
+
+        //id를 조회하여 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //id 값으로 데이터를 찾을 때 해당 id 값이 없으면 null을 반환하라는 뜻
+
+        //모델에 데이터 등록(MVC 패턴에 따라 데이터를 뷰 페이지에서 사용하기 위함)
+        model.addAttribute("article", articleEntity);
+
+        //뷰 페이지 반환
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+
+        //모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+
+        return "articles/index";
     }
 
 }
--- src/main/java/com/example/firstproject/entitiy/Article.java
+++ src/main/java/com/example/firstproject/entitiy/Article.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor //클래스 안쪽의 모든 필드를 매개변수로 하는 생성자를 자동으로 만들어주는 어노테이션(lombok사용시)
+@NoArgsConstructor //기본 생성자 추가 어노테이션 (객체 생성시 내부적으로 기본 생성자로 생성하고 setter로 필드 값을 설정한다고함, 따라서 반드시 명시해야됨)
 @ToString //toString() 메소드를 자동으로 생성해주는 어노테이션(lombok사용시)
 @Entity //엔티티 선언
 public class Article {
